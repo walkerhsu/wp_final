a16d57c9f8cfe2db34301b489a0e709c22c11c17
David Liu <david551158@gmail.com>
2022-12-08 16:25:15 +0800

    frontend drawer added

--- frontend/src/containers/pages/MainPage.js
+++ frontend/src/containers/pages/MainPage.js
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import {Button, } from "@material-ui/core"; 
 import '../../css/MainPage.css'
 // import LoginIcon from '@mui/icons-material/Login';
+import TemporaryDrawer from "../../components/Drawer";
+
 
 const MainPage = () => {
     // const avatarStyle={backgroundColor:'#1bbd7e'}
@@ -16,10 +18,13 @@ const MainPage = () => {
         <div className = 'MainPageWrapper'>
             <div class="bg-image"/>
             <div className = "LogoWrapper">
-                <h1>Main Page</h1>
-                <div class="wave one"></div>
-                <div class="wave two"></div>
-                <div class="wave three"></div>
+                <div className="Logo">
+                    <h1>Main Page</h1>
+                    <div class="wave one"></div>
+                    <div class="wave two"></div>
+                    <div class="wave three"></div>
+                    <TemporaryDrawer className="drawer" />
+                </div>
             </div>
             <div className = "ButtonWrapper">
                 <h1>Login to Your Account</h1>
