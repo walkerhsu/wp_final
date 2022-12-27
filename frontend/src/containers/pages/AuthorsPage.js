59dfa2546b10910ca53c9fd91e2e5abd1c8133bf
David Liu <david551158@gmail.com>
2022-12-27 16:10:23 +0800

    authors page updated

--- frontend/src/containers/pages/AuthorsPage.js
+++ frontend/src/containers/pages/AuthorsPage.js
@@ -1,6 +1,53 @@
+import "../../css/AuthorsPage.css"
 const AuthorsPage = () => {
-    return <>
-        <h1>Authors page</h1>
-    </>
+    return <div className="Wrapper">
+        <div className="about-section">
+        <h1>About Us Page</h1>
+        <p>Some text about who we are and what we do.</p>
+        <p>Resize the browser window to see that this page is responsive by the way.</p>
+        </div>
+
+        <h2 style={{textAlign: "center"}}>Our Team</h2>
+        <div className="row">
+            <div className="column">
+                <div className="card">
+                {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
+                    <div className="container">
+                        <h2>Jane Doe</h2>
+                        <p className="title">CEO & Founder</p>
+                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                        <p>jane@example.com</p>
+                        <p><button className="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                {/* <img src="/w3images/team2.jpg" alt="Mike" style="width:100%"> */}
+                    <div className="container">
+                        <h2>Mike Ross</h2>
+                        <p className="title">Art Director</p>
+                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                        <p>mike@example.com</p>
+                        <p><button className="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                {/* <img src="/w3images/team3.jpg" alt="John" style="width:100%"> */}
+                    <div className="container">
+                        <h2>John Doe</h2>
+                        <p className="title">Designer</p>
+                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                        <p>john@example.com</p>
+                        <p><button className="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 export default AuthorsPage;
