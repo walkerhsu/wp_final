ffe57db6b9ed84fc52ffe2bd413eb86caee2ba18
David Liu <david551158@gmail.com>
2022-12-31 14:44:48 +0800

    frontend about us picture added

--- frontend/src/containers/pages/AboutUs/AuthorsPage.js
+++ frontend/src/containers/pages/AboutUs/AuthorsPage.js
@@ -1,4 +1,7 @@
 import "../../../css/AuthorsPage.css"
+import person1 from "../../../images/person1.png"
+import person2 from "../../../images/person2.png"
+import person3 from "../../../images/person3.png"
 const AuthorsPage = () => {
     return <div className="Wrapper">
         <div className="about-section">
@@ -11,39 +14,45 @@ const AuthorsPage = () => {
         <div className="row">
             <div className="column">
                 <div className="card">
-                {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
+                <img src={person1} alt="Ryan" style={{width:70 + '%'}} />
                     <div className="container">
-                        <h2>Jane Doe</h2>
-                        <p className="title">CEO & Founder</p>
-                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                        <p>jane@example.com</p>
+                        <h1>Ryan Chu</h1>
+                        <h2 className="title">Backend Developer</h2>
+                        <h3>Some text.</h3>
+                        <h3>ryan@example.com</h3>
+                        <br></br>
                         <p><button className="button">Contact</button></p>
+                        <br></br>
                     </div>
                 </div>
             </div>
 
             <div className="column">
                 <div className="card">
-                {/* <img src="/w3images/team2.jpg" alt="Mike" style="width:100%"> */}
+                <img src={person2} alt="Walker" style={{width:70 + '%'}} />
                     <div className="container">
-                        <h2>Mike Ross</h2>
-                        <p className="title">Art Director</p>
-                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                        <p>mike@example.com</p>
+                        <h1>Walker Hsu</h1>
+                        <h2 className="title">Frontend Developer</h2>
+                        <h3>Some text.</h3>
+                        <h3>walker@example.com</h3>
+                        <br></br>
                         <p><button className="button">Contact</button></p>
+                        <br></br>
                     </div>
                 </div>
             </div>
 
             <div className="column">
                 <div className="card">
-                {/* <img src="/w3images/team3.jpg" alt="John" style="width:100%"> */}
+                <img src={person3} alt="David" style={{width:70 + '%'}} />
                     <div className="container">
-                        <h2>John Doe</h2>
-                        <p className="title">Designer</p>
-                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                        <p>john@example.com</p>
+                        <h1>David Liu</h1>
+                        <h2 className="title">HomePage Designer</h2>
+                        <h3>Some text.</h3>
+                        <h3>david@example.com</h3>
+                        <br></br>
                         <p><button className="button">Contact</button></p>
+                        <br></br>
                     </div>
                 </div>
             </div>
