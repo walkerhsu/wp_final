fd73aef81895083d4751441605ac3e27ed02496d
Ryan Chu <ryan@RyandeMBP.Home>
2023-01-02 01:14:47 +0800

    import bar

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { 
+  Chart as ChartJS,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
 import { useAccount } from "../containers/hooks/useAccount";
 import { getPieChartData } from "../utils/getPieChartData";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 function PieChart() {
   const { accountData } = useAccount();
@@ -22,7 +39,50 @@ function PieChart() {
           }}
         />
       </div>
+      <h2 style={{ textAlign: "center" }}>Monthly Analysis</h2>
+      <div>
+        <Bar
+          data={chartData}
+          // Height of graph
+          height={400}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    // The y-axis value will start from zero
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              labels: {
+                fontSize: 15,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
+/*data={{
+            // Name of the variables on x-axies for each bar
+            labels: ["1st bar", "2nd bar", "3rd bar", "4th bar"],
+            datasets: [
+              {
+                // Label for bars
+                label: "total count/value",
+                // Data or value of your each variable
+                data: [1552, 1319, 613, 1400],
+                // Color of each bar
+                backgroundColor: ["aqua", "green", "red", "yellow"],
+                // Border color of each bar
+                borderColor: ["aqua", "green", "red", "yellow"],
+                borderWidth: 0.5,
+              },
+            ],
+          }} */
 export default PieChart;
