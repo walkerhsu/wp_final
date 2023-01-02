913b6c2c2ede79495570328e9fcc16d2b4d8291a
David Liu <david551158@gmail.com>
2023-01-02 17:39:17 +0800

    frontend contact page

--- frontend/src/containers/pages/AboutUs/ContactPage.js
+++ frontend/src/containers/pages/AboutUs/ContactPage.js
@@ -1,6 +1,12 @@
+import "../../../css/ContactPage.css"
+import FormDialog from "../../../components/FormDialog";
 const ContactPage = () => {
-    return <>
-        <h1>Contact page</h1>
-    </>
+    return <div className="Wrapper">
+        <div className="dialogBox" />
+        <div className="messageWrapper">
+            <div className="textBox">Your comments and suggestions are welcome ! ! !</div>
+            <FormDialog />
+        </div>
+    </div>
 }
 export default ContactPage;
