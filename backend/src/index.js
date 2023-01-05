ddd54e1b3532a4af8ecea55512fc62876bddf05a
walkerhsu <walkerhsu0808@gmail.com>
2023-01-05 19:37:27 +0800

    final_project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 import mongo from "./mongo.js";
-import app from "./server.js";
+import httpServer from "./server.js";
 
 import "dotenv-defaults/config.js";
 
 mongo.connect();
 const port = process.env.PORT | 4000;
 
-app.listen({ port }, () => {
+httpServer.listen({ port }, () => {
   console.log(`The server is up on port ${port}!`);
 });
